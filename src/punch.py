626b40fafca10ec5f4cf88add26f750947d3363c
William Lebel <william.leb@hotmail.com>
2019-05-28 14:43:43 -0400

    [0.0.2] Now, we can add a time to the cache.

--- src/punch.py
+++ src/punch.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import datetime
+from enum import Enum
 
 
 TIME_PUNCHER_DIR_NAME = '.time-puncher'
+TIME_PUNCHER_CACHE_FILE_NAME = 'time-cache'
+BACKUP_DIR_NAME = 'backups'
+
+TIME_PUNCHER_DIR = os.path.join(os.environ['HOME'], TIME_PUNCHER_DIR_NAME)
+CACHE_PATH = os.path.join(os.environ['HOME'], TIME_PUNCHER_DIR_NAME, TIME_PUNCHER_CACHE_FILE_NAME)
+BACKUP_DIR = os.path.join(TIME_PUNCHER_DIR, BACKUP_DIR_NAME)
 
 MINUTES_TO_HOURS = 1 / 60
 HOURS_TO_MINUTES = 60
+
+
+class ConsoleColor:
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    YELLOW = '\033[93m'
+    END = '\033[0m'
 
 
 class HourFormatError(ValueError):
@@ -15,13 +30,17 @@ class HourFormatError(ValueError):
 
 def init():
     make_time_puncher_dir()
+    make_backup_dir()
 
 
 def make_time_puncher_dir():
-    time_puncher_dir = os.path.join(os.environ['HOME'], TIME_PUNCHER_DIR_NAME)
+    if not os.path.exists(TIME_PUNCHER_DIR):
+        os.mkdir(TIME_PUNCHER_DIR)
 
-    if not os.path.exists(time_puncher_dir):
-        os.mkdir(time_puncher_dir)
+
+def make_backup_dir():
+    if not os.path.exists(BACKUP_DIR):
+        os.mkdir(BACKUP_DIR)
 
 
 def run():
@@ -43,7 +62,8 @@ def parse_args():
     parser.add_argument('-s, --save', dest='save', action='store_true',
                         help="Use this flag to save the currently stored times. It also erases every stored times.")
 
-    parser.add_argument('time', nargs='?')
+    parser.add_argument('time', nargs='?',
+                        help="Time to add in cache.")
 
     return parser.parse_args()
 
@@ -58,13 +78,40 @@ def verify_args(args):
 def save_times(output_file):
     print("Saving file at {}".format(output_file))  # TODO
 
+    times = read_cached_times()
+
+    working_time_spans = []
+    while len(times) > 1:
+        working_time_spans.append((times.pop(0), times.pop(0)))
+
+    if len(times) == 1:
+        log_err("The time {} is not in pair and thus will be ignored.".format(times[0]))
+
+
+def clear_cache():
+    open(CACHE_PATH, 'w').close()
+
 
 def add_time(time_to_add):
     """
     :type time_to_add: str
     """
+    times = read_cached_times()
+    with open(CACHE_PATH, 'w') as cache:
+        times.append(parse_time(time_to_add))
+        cache.writelines(["{}\n".format(time) for time in sorted(times)])
 
-    print("Adding time: {}".format(parse_time(time_to_add)))  # TODO
+    log_info("Added time {} to the cache.".format(time_to_add))
+
+
+def read_cached_times():
+    try:
+        with open(CACHE_PATH, 'r') as cache:
+            times = [parse_time(time) for time in cache.readlines()]  # TODO Manage error
+
+        return times
+    except FileNotFoundError:
+        return []
 
 
 def parse_time(time_to_parse):
@@ -82,6 +129,38 @@ def parse_time(time_to_parse):
             return float(time_to_parse)
         except ValueError as e:
             raise HourFormatError(e)
+
+
+def format_time(time_to_format):
+    """
+    :type time_to_format: float
+    :rtype: str
+    """
+    hours = int(time_to_format)
+    minutes = (time_to_format - hours) * HOURS_TO_MINUTES
+
+    return "{}:{}".format(hours, minutes)
+
+
+def log_info(str):
+    """
+    :type str: str
+    """
+    print("{}{}{}".format(ConsoleColor.GREEN, str, ConsoleColor.END))
+
+
+def log_err(str):
+    """
+    :type str: str
+    """
+    print("{}{}{}".format(ConsoleColor.RED, str, ConsoleColor.END))
+
+
+def log_warn(str):
+    """
+    :type str: str
+    """
+    print("{}{}{}".format(ConsoleColor.YELLOW, str, ConsoleColor.END))
 
 
 if __name__ == '__main__':
