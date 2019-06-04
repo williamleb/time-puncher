dae20876306a47e8e23cf24a65167f081f836e66
William Lebel <william.leb@hotmail.com>
2019-06-04 08:58:39 -0400

    - Verifying arguments int the add punch module. - Moving the writing of time in the cache from the add punch module to the cache utilities.

--- src/punch_modules/add.py
+++ src/punch_modules/add.py
@@ -1,38 +1,47 @@
 import argparse
 
-from src.utils.cache_utils import CACHE_PATH, read_cached_times
-from src.utils.log_utils import log_info
+from src.utils.cache_utils import read_cached_times, write_times
+from src.utils.errors import HourFormatError
+from src.utils.log_utils import log_info, log_err
 from src.utils.time_utils import parse_time
 
 
 def run():
     args = _parse_args()
 
-    # TODO Verify Args
+    if not _verify_args(args):
+        exit(1)
 
     _add_time(args.time)
 
 
 def _parse_args():
-    parser = argparse.ArgumentParser(description="Adds a time to the cache.")  # TODO Better description
+    parser = argparse.ArgumentParser(description="Adds a time for the day.")
 
     parser.add_argument('time',
-                        help="Time to add in cache.")
+                        help="Time to add for the day.")
 
     return parser.parse_args()
 
 
 def _verify_args(args):
-    return True  # TODO
+    # Verification 1: Time must be parsable (in a valid format).
+    try:
+        parse_time(args.time)
+    except HourFormatError:
+        log_err("")
+        return False
+
+    return True
 
 
 def _add_time(time_to_add):
     """
     :type time_to_add: str
     """
+
     times = read_cached_times()
-    with open(CACHE_PATH, 'w') as cache:  # TODO Method in cache_util to write in cache
-        times.append(parse_time(time_to_add))
-        cache.writelines(["{}\n".format(time) for time in sorted(times)])
+    times.append(parse_time(time_to_add))
+    write_times(times)
 
     log_info("Added time {} to the cache.".format(time_to_add))
--- src/utils/cache_utils.py
+++ src/utils/cache_utils.py
@@ -3,16 +3,29 @@ import os
 from src.punch import TIME_PUNCHER_DIR_NAME  # TODO Place that const in another file maybe
 from src.utils.time_utils import parse_time
 
-TIME_PUNCHER_CACHE_FILE_NAME = 'time-cache'
+_TIME_PUNCHER_CACHE_FILE_NAME = 'time-cache-lol'
 
-CACHE_PATH = os.path.join(os.environ['HOME'], TIME_PUNCHER_DIR_NAME, TIME_PUNCHER_CACHE_FILE_NAME)
+_CACHE_PATH = os.path.join(os.environ['HOME'], TIME_PUNCHER_DIR_NAME, _TIME_PUNCHER_CACHE_FILE_NAME)
 
 
 def read_cached_times():
+    """
+    :rtype: list of float
+    """
     try:
-        with open(CACHE_PATH, 'r') as cache:
+        with open(_CACHE_PATH, 'r') as cache:
             times = [parse_time(time) for time in cache.readlines()]  # TODO Manage error
 
         return times
     except FileNotFoundError:
         return []
+
+
+def write_times(times):
+    """
+    :type times: list of float
+    """
+
+    with open(_CACHE_PATH, 'w') as cache:
+        cache.writelines(["{}\n".format(time) for time in sorted(times)])
+
